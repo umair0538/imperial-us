3ad89dcd73be56856e4519a45a0dbe3aa7ff96c8
Umair Shahid <umair@192.168.1.10>
2026-07-27 01:04:35 +0500

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${cormorant.variable}`}
       >
+        <Analytics />
         <Navbar />
           {children}
         <Footer />
